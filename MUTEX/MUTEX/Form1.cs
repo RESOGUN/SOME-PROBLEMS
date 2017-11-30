7c3b831c4601635090cc9d7e806b6e4800898b2d
RESOGUN <32155157+RESOGUN@users.noreply.github.com>
2017-11-30 03:26:26 +0200

    Add files via upload

---
+++ MUTEX/MUTEX/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MUTEX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static class Program
+        {
+           
+            
+        }
+
+    }
+}
